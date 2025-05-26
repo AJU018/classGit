79e394c4579e5426e2c9a3308c31182127b08f01
ALD ZUKHRUF <aldzukhruf@gmail.com>
2025-05-26 14:36:31 +0800

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbName = "buku";
-$dbHost = "localhost";
-$dbUser = "root";
-$dbPass = "";
-$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-if (!$conn) {
-    die("Pangkalan data tidak dapat disambungkan.");
-}
